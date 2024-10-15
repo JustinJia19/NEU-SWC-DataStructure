9cb6636c0c27933d4a70427924715274a32ab29c
JustinJia19 <142551939+JustinJia19@users.noreply.github.com>
2024-10-15 13:17:17 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,28 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "Stack.h"
-
-void conversation()
+void getChar(char *input, char* output)
 {
-	SqStack S;
-	int n;
-	InitStack(&S);
-	scanf_s("%d", &n);
-	while (n)
+	int i, j = 0;
+	for (i = 0; input[i] != '\0'; i++)
 	{
-		Push(&S, n%8);
-		n = n / 8;
+		if (input[i] != ' ')
+		{
+			output[j] = input[i];
+			j++;
+		}
 	}
-	while (!StackEmpty(&S))
-	{
-		int e = 0;
-		Pop(&S, &e);
-		printf("%d", e);
-	}
+	output[j] = '\0';
 }
-int main(void)
+int main()
 {
+	char input[100];
+	char output[100];
+	SqStack S;
+	int label = 0;
+	
+	printf("请输入字符串：\n");
+	fgets(input, sizeof(input), stdin);
+	input[strcspn(input, "\n")] = '\0';
+	getChar(input, output);
+	InitStack(&S);
+	for (int i = 0; output[i] != '\0'; i++)
+	{
+		Push(&S, output[i]);
+	}
+	for (int i = 0; output[i] != '\0'; i++)
+	{
+		char find = 0;
+		Pop(&S, &find);
+		if (find != output[i])
+		{
+			label = 1;
+			break;
+		}
+	}
+	if (label == 1)
+		printf("不是回文！");
+	else
+		printf("是回文！");
 
-	conversation();
 	return 0;
 }
